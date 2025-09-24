c858110fbc24f000bcdc92164635933553f7e856
Kje_Axic <122368368+KaFuuchao0313@users.noreply.github.com>
2025-09-24 09:31:38 +0800

    Add files via upload

--- salmon world blueprint.c
+++ salmon world blueprint.c
@@ -17,9 +17,20 @@ void slowprint(const char *text,int delay_ms)
 }
 void main()
 {
+    char ans;
     slowprint("the story is now loading",100);
     printf("\nsalmon:\n");
     slowprint("列车马上就要到站了，请做出你的选择。",50);
-    printf("\nA.如果可以的话，请让我一直留在你身边。\nB.好吧，看来我别无选择。");
-    /*return 0;*/
+    printf("\nA.如果可以的话，请让我一直留在你身边。\nB.好吧，看来我别无选择。\n\n");
+    scanf("%c",&ans);
+    if(ans=='A'){
+        printf("\nsalmon:\n");
+        slowprint("我的荣幸。",50);
+    }
+    else if(ans=='B'){
+        printf("\nsalmon:\n");
+        slowprint("看来列车长想要把我赶下车了呢。",50);
+    }
+    else;
+    return 0;
 }
